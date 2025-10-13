3b6b0ec863e327d35a850fb099f61fae0387a87f
shurov <shurov.0.00.0@gmail.com>
2025-10-13 15:11:04 +0600

    error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 // next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
+    // Allow external avatars served from backend with port
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '192.168.7.12',
+        port: '4001',
+        pathname: '/**',
+      },
+    ],
     domains: ['randomuser.me'],
   },
-}
+};
+
+module.exports = nextConfig;
--- public/sidebar/icons/CollapseIcon.tsx
+++ public/sidebar/icons/CollapseIcon.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function CollapseIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-  <path d="M21.9711 14.9998V8.99981C21.9711 3.99981 19.9711 1.99981 14.9711 1.99981H8.9711C3.9711 1.99981 1.9711 3.99981 1.9711 8.99981V14.9998C1.9711 19.9998 3.9711 21.9998 8.9711 21.9998H14.9711C19.9711 21.9998 21.9711 19.9998 21.9711 14.9998Z" stroke="#A5A5AB" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M7.9711 1.99981V21.9998" stroke="#A5A5AB" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M14.97 9.44L12.41 12L14.97 14.56" stroke="#A5A5AB" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M21.9711 14.9998V8.99981C21.9711 3.99981 19.9711 1.99981 14.9711 1.99981H8.9711C3.9711 1.99981 1.9711 3.99981 1.9711 8.99981V14.9998C1.9711 19.9998 3.9711 21.9998 8.9711 21.9998H14.9711C19.9711 21.9998 21.9711 19.9998 21.9711 14.9998Z" stroke="#A5A5AB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M7.9711 1.99981V21.9998" stroke="#A5A5AB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+  <path d="M14.97 9.44L12.41 12L14.97 14.56" stroke="#A5A5AB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
   )
 }
